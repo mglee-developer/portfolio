effa6830f5ae8a75a438fcc8b6484cc9e84337cd
ming <mglee.developer@gmail.com>
2020-08-26 15:59:57 +0900

    JS : home transparent

--- main.js
+++ main.js
@@ -33,3 +33,10 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+// home transparent
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+});
