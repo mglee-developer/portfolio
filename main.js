5b44a3c325ac14c9292fdf66b8591a88c6d24fad
ming <mglee.developer@gmail.com>
2020-08-26 15:29:44 +0900

    JS : scroll section + handle btn

--- main.js
+++ main.js
@@ -10,3 +10,26 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// when we click menu, scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+
+    scrollIntoView(link);
+});
+
+// when we click 'clickme' button, scrolling
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (event) => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
